7bf29f13650e0dffbd6c39315e8d0a4a32a8ab41
Colin Noga <Tempus@inbox.com>
2014-12-15 21:14:54 -0700

    Entity stacking indicator fix 3

--- BasementRenovator.py
+++ BasementRenovator.py
@@ -130,7 +130,7 @@ class RoomScene(QGraphicsScene):
 
 				if x[1] > 1:
 					painter.drawRect( (x[0] + 1) * 26 - 8, (y[0] + 1) * 26 - 8, 8, 8)
-					painter.drawText( (x[0] + 1) * 26 - 7, (y[0] + 1) * 26 -1, str(y[1]) )
+					painter.drawText( (x[0] + 1) * 26 - 7, (y[0] + 1) * 26 -1, str(x[1]) )
 
 		# Grey out the screen to show it's inactive if there are no rooms selected
 		if mainWindow.roomList.selectedRoom() is None:
