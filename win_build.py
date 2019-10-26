1d730d9ef0c01497150496ec06c8a89b9a317bed
budj <budjmt@aol.com>
2019-10-26 09:34:14 -0400

    fix window build script
    Run with python win_build.py

--- win_build.py
+++ win_build.py
@@ -1,29 +1,47 @@
-from distutils.core import setup
-import py2exe, sys, os, shutil
-
-sys.argv.append('py2exe')
+import sys, subprocess, shutil, os
 
 if os.path.exists('dist'):
-	shutil.rmtree('dist')
+    shutil.rmtree('dist')
 
-setup(
-	name="Basement Renovator",
-	version="0.1",
-	author="Tempus",
-	author_email="Tempus@chronometry.ca",
-	windows = [{'script': "BasementRenovator.py"}],
-	options = {'py2exe': {
-		'bundle_files': 1, 
-		'compressed': False, 
-		"includes":["sip", "xml.etree"],
-		'optimize': 2,
-		'bundle_files': 3,
-		"icon_resources": [(0, "resources/UI/BasementRenovator.ico")]
-	}}
-)
+pydir = os.path.split(sys.executable)[0]
 
+def exe(pyfile,name="",dest="",creator="PyInstaller",ico="",noconsole=False,versionFile="",addtlFiles=[]):
+    args = [
+        'python', '-m', creator, pyfile, '-F', '--clean',
+        '--distpath', str(dest or os.path.split(pyfile)[0])
+    ]
+    if name: args.extend(('-n', name))
+    if ico: args.extend(('-i', ico))
+    if versionFile: args.extend(('--version-file', versionFile))
+    if noconsole: args.append('--noconsole')
 
-os.mkdir('dist/platforms')
-shutil.copytree('resources', 'dist/resources')
-shutil.copy('C:\\Python34/Lib/site-packages/PyQt5/plugins/platforms/qwindows.dll', 'dist/platforms')
-shutil.copy('C:\\Python34/Lib/site-packages/PyQt5/libEGL.dll', 'dist')
+    for ty, src, dst in addtlFiles:
+        args.extend((ty == 'binary' and '--add-binary' or '--add-data', f'{os.path.normpath(src)};{os.path.normpath(dst)}'))
+
+    print(args)
+    subprocess.check_output(args)
+
+# TODO once log files are added, disable the console
+exe('BasementRenovator.py',
+    name="Basement Renovator",
+    dest="dist",
+    versionFile="winversion.txt",
+    ico=os.path.normpath("resources/UI/BasementRenovator.ico"),
+    addtlFiles=[
+        ('binary', os.path.join(pydir, 'Lib/site-packages/PyQt5/Qt/plugins/platforms/qwindows.dll'), './platforms'),
+        ('binary', os.path.join(pydir, 'Lib/site-packages/PyQt5/Qt/bin/libEGL.dll'), '.')
+    ])
+
+addtl = [
+    ('resources', ''),
+    ('README.md', ''),
+    ('NicheFeatures.md', ''),
+]
+
+for src, dest in addtl:
+    dest = os.path.join('dist', dest, os.path.split(src)[1])
+    print(dest)
+    if os.path.isdir(src):
+        shutil.copytree(src, dest)
+    else:
+        shutil.copy2(src, dest)
