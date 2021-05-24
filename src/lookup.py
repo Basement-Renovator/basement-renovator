51decc2a84498906f1bfff315135b9370685cb85
snake <snake.block.10@gmail.com>
2021-05-24 08:18:47 +0500

    reverted changes to lookup.py and StagesRepentance.xml
    probably wasn't worth keepin these

--- src/lookup.py
+++ src/lookup.py
@@ -417,11 +417,10 @@ class MainLookup:
             raise ValueError("Invalid gfx node!", node.tag, node.attrib)
 
         for ent in node.findall("Entity"):
-            idList = ent.get("ID").split(",")
-
-            for id in idList:
-                entid = f"{id}.{ent.get('Variant', '0')}.{ent.get('SubType', '0')}"
-                entities[entid] = ent
+            entid = (
+                f"{ent.get('ID')}.{ent.get('Variant', '0')}.{ent.get('SubType', '0')}"
+            )
+            entities[entid] = ent
 
         ret = {"Paths": paths, "Entities": entities}
 
