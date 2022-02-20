900ca1270fa491ef7d6960f500a70531176ddf5f
crossstoney <96753321+crossstoney@users.noreply.github.com>
2022-02-20 09:26:23 -0500

    Formatted In Black

--- resources/Notes/entityGrouping.py
+++ resources/Notes/entityGrouping.py
@@ -2,28 +2,28 @@ import struct, os
 import xml.etree.ElementTree as ET
 
 
-tree = ET.ElementTree(file='resources/Entities.xml')
+tree = ET.ElementTree(file="resources/Entities.xml")
 entityXML = tree.getroot()
 
-eList = entityXML.findall('entity')
+eList = entityXML.findall("entity")
 
 for e in eList:
 
-	e.set('Group', 'All')
-	e.set('Kind', 'Misc')
+    e.set("Group", "All")
+    e.set("Kind", "Misc")
 
-	# Groups
-	if e.get('ID') == "5":
-		e.set('Kind', 'Pickups')
+    # Groups
+    if e.get("ID") == "5":
+        e.set("Kind", "Pickups")
 
-	if (int(e.get('ID')) > 9) and (int(e.get('ID')) < 1000):
-		e.set('Kind', 'Enemies')
+    if (int(e.get("ID")) > 9) and (int(e.get("ID")) < 1000):
+        e.set("Kind", "Enemies")
 
-	if e.get('Boss') == "1":
-		e.set('Kind', 'Bosses')
+    if e.get("Boss") == "1":
+        e.set("Kind", "Bosses")
 
-	if int(e.get('ID')) > 999:
-		e.set('Kind', 'Grid')
+    if int(e.get("ID")) > 999:
+        e.set("Kind", "Grid")
 
 
-tree.write('resources/Entities2.xml')
+tree.write("resources/Entities2.xml")
--- win_build.py
+++ win_build.py
@@ -1,45 +1,84 @@
 import sys, subprocess, shutil, os
 
-if os.path.exists('dist'):
-    shutil.rmtree('dist')
+if os.path.exists("dist"):
+    shutil.rmtree("dist")
 
 pydir = os.path.split(sys.executable)[0]
 
-def exe(pyfile,name="",dest="",creator="PyInstaller",ico="",noconsole=False,versionFile="",addtlFiles=[]):
+
+def exe(
+    pyfile,
+    name="",
+    dest="",
+    creator="PyInstaller",
+    ico="",
+    noconsole=False,
+    versionFile="",
+    addtlFiles=[],
+):
     args = [
-        'python', '-u', '-m', creator, pyfile, '-F', '--clean',
-        '--distpath', str(dest or os.path.split(pyfile)[0])
+        "python",
+        "-u",
+        "-m",
+        creator,
+        pyfile,
+        "-F",
+        "--clean",
+        "--distpath",
+        str(dest or os.path.split(pyfile)[0]),
     ]
-    if name: args.extend(('-n', name))
-    if ico: args.extend(('-i', ico))
-    if versionFile: args.extend(('--version-file', versionFile))
-    if noconsole: args.append('--noconsole')
+    if name:
+        args.extend(("-n", name))
+    if ico:
+        args.extend(("-i", ico))
+    if versionFile:
+        args.extend(("--version-file", versionFile))
+    if noconsole:
+        args.append("--noconsole")
 
     for ty, src, dst in addtlFiles:
-        args.extend((ty == 'binary' and '--add-binary' or '--add-data', f'{os.path.normpath(src)};{os.path.normpath(dst)}'))
+        args.extend(
+            (
+                ty == "binary" and "--add-binary" or "--add-data",
+                f"{os.path.normpath(src)};{os.path.normpath(dst)}",
+            )
+        )
 
     print(args)
     subprocess.check_output(args)
 
+
 # TODO once log files are added, disable the console
-exe('BasementRenovator.py',
+exe(
+    "BasementRenovator.py",
     name="Basement Renovator",
     dest="dist",
     versionFile="winversion.txt",
     ico=os.path.normpath("resources/UI/BasementRenovator.ico"),
     addtlFiles=[
-        ('binary', os.path.join(pydir, 'Lib/site-packages/PyQt5/Qt5/plugins/platforms/qwindows.dll'), './platforms'),
-        ('binary', os.path.join(pydir, 'Lib/site-packages/PyQt5/Qt5/bin/libEGL.dll'), '.')
-    ])
+        (
+            "binary",
+            os.path.join(
+                pydir, "Lib/site-packages/PyQt5/Qt5/plugins/platforms/qwindows.dll"
+            ),
+            "./platforms",
+        ),
+        (
+            "binary",
+            os.path.join(pydir, "Lib/site-packages/PyQt5/Qt5/bin/libEGL.dll"),
+            ".",
+        ),
+    ],
+)
 
 addtl = [
-    ('resources', ''),
-    ('README.md', ''),
-    ('NicheFeatures.md', ''),
+    ("resources", ""),
+    ("README.md", ""),
+    ("NicheFeatures.md", ""),
 ]
 
 for src, dest in addtl:
-    dest = os.path.join('dist', dest, os.path.split(src)[1])
+    dest = os.path.join("dist", dest, os.path.split(src)[1])
     print(dest)
     if os.path.isdir(src):
         shutil.copytree(src, dest)
