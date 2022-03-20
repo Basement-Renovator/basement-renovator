2c9ff17389efd05179cea5448aacec7344936978
James <5511220+Zamiell@users.noreply.github.com>
2022-03-20 17:06:20 -0400

    autoformat xml (#281)
    * modifying gitignore, also changing line endings
    
    * fixes for formatting
    
    * updating docs
    
    * adding linter
    
    * format xml on save
    
    * adding prettier to recommended extensions
    
    * autoformatting all XML files
    
    * removing dead code
    
    * fix for old version of black
    
    * adding long lines
    
    * adding gitattributes
    
    * fixing line endings

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// This is the configuration file for Prettier, the auto-formatter
+// https://prettier.io/docs/en/configuration.html
+module.exports = {
+  endOfLine: "lf",
+  printWidth: 1000000,
+
+  // https://github.com/prettier/plugin-xml#configuration
+  // The default is "struct"
+  // However, whitespace cannot be reformatted unless this is set to "ignore"
+  xmlWhitespaceSensitivity: "ignore",
+};
