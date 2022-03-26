fe36e9dd77b296fe312f099e86d7269be30930a2
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-26 13:00:32 -0400

    adding publish helper script

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e # Exit on any errors
+
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# CI is set up to automatically generate a release if the word "release" is in a commit name,
+# so we make and empty commit with that name and push
+cd "$DIR"
+git commit --allow-empty -m "release"
+git push
