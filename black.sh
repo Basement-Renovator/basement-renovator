79ca4a48658feaad05770dad440d9bc65404bb4b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-26 13:58:29 -0400

    bugfixes for black.sh

--- black.sh
+++ black.sh
@@ -6,13 +6,13 @@ set -e # Exit on any errors
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-$GITHUB_ORIGIN_URL=$1
+GITHUB_ORIGIN_URL=$1
 if [ -z "$GITHUB_ORIGIN_URL" ]; then
   echo "Error: You must provide the GitHub origin URL as the first argument."
   exit 1
 fi
 
-$GITHUB_HEAD_REF=$2
+GITHUB_HEAD_REF=$2
 if [ -z "$GITHUB_HEAD_REF" ]; then
   echo "Error: You must provide the GitHub head reference as the second argument."
   exit 1
