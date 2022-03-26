99bb24b65c203e132f3aabdb52f09f25ddabfe15
autoblack <black@users.noreply.github.com>
2022-03-26 17:47:00 +0000

    fixup: Format Python code with Black

--- BasementRenovator.py
+++ BasementRenovator.py
@@ -99,7 +99,9 @@ def findInstallPath():
             if not basePath:
                 cantFindPath = True
             else:
-                installPath = os.path.join(basePath, "steamapps", "common", "The Binding of Isaac Rebirth")
+                installPath = os.path.join(
+                    basePath, "steamapps", "common", "The Binding of Isaac Rebirth"
+                )
                 if not QFile.exists(installPath):
                     cantFindPath = True
 
@@ -3818,11 +3820,11 @@ class ReplaceDialog(QDialog):
             layout = QFormLayout()
 
             self.type = QSpinBox()
-            self.type.setRange(1, 2 ** 31 - 1)
+            self.type.setRange(1, 2**31 - 1)
             self.variant = QSpinBox()
-            self.variant.setRange(-1, 2 ** 31 - 1)
+            self.variant.setRange(-1, 2**31 - 1)
             self.subtype = QSpinBox()
-            self.subtype.setRange(-1, 2 ** 8 - 1)
+            self.subtype.setRange(-1, 2**8 - 1)
 
             layout.addRow("&Type:", self.type)
             layout.addRow("&Variant:", self.variant)
@@ -4531,7 +4533,7 @@ class StatisticsDialog(QDialog):
         filterBoxLayout.addWidget(appearCountThresholdLabel)
 
         self.appearCountThresholdSpinner = QSpinBox()
-        self.appearCountThresholdSpinner.setRange(1, (2 ** 31) - 1)
+        self.appearCountThresholdSpinner.setRange(1, (2**31) - 1)
         self.appearCountThresholdSpinner.valueChanged.connect(self.refresh)
         filterBoxLayout.addWidget(self.appearCountThresholdSpinner)
 
@@ -6110,7 +6112,7 @@ class MainWindow(QMainWindow):
 
                 # Set the selected rooms to have descending ids from max
                 # this should avoid any id conflicts
-                baseId = (2 ** 31) - 1
+                baseId = (2**31) - 1
                 newRooms = list(
                     Room(
                         f"{room.name} [Real ID: {room.info.variant}]",
