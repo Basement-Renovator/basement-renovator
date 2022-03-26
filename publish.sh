ce80b92f040506b7307fe092f1c18d9d7de055c2
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-26 13:09:15 -0400

    better publish script

--- publish.sh
+++ publish.sh
@@ -9,5 +9,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # CI is set up to automatically generate a release if the word "release" is in a commit name,
 # so we make and empty commit with that name and push
 cd "$DIR"
+
+# Don't proceed if the repo is dirty
+GIT_STATUS_OUPUT=$(git status --porcelain)
+if [ ! -z "$GIT_STATUS_OUPUT" ]; then
+  echo "Error: You cannot publish a new release if the repository is not clean. Make a separate commit with any changes that you have pending."
+  exit 1
+fi
+
 git commit --allow-empty -m "release"
 git push
+echo "Pushed an empty commit to inform CI to create a new release."
